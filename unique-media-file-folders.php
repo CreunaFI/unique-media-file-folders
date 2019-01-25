724097c9ddc867f732ae80e70cc5fc9187f28110
Johannes Siipola <johannes@siipo.la>
2019-01-26 00:07:56 +0200

    Initial commit

---
+++ unique-media-file-folders.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin name: Unique Media File Folders
+Plugin URI: https://github.com/joppuyo/unique-media-file-folders
+Description: Give all media files and their thumbnails their own unique folder
+Version: 0.0.1
+Author: Johannes Siipola
+Author URI: https://siipo.la
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Check if we are using local Composer
+if (file_exists(__DIR__ . '/vendor')) {
+    require 'vendor/autoload.php';
+}
+
+add_action('plugins_loaded', function () {
+    add_filter('wp_handle_upload_prefilter', 'umff_pre_upload');
+
+    add_filter('wp_handle_upload', 'umff_post_upload');
+});
+
+function umff_pre_upload($file)
+{
+    add_filter('upload_dir', 'umff_custom_upload_dir');
+    return $file;
+}
+
+function umff_post_upload($file_info)
+{
+    remove_filter('upload_dir', 'umff_custom_upload_dir');
+    return $file_info;
+}
+
+function umff_custom_upload_dir($path)
+{
+    if (!empty($path['error'])) {
+        return $path;
+    }
+
+    $uuid4 = \Ramsey\Uuid\Uuid::uuid4();
+    $uuid = $uuid4->getBytes();
+
+    $base32 = new Base2n(5, 'abcdefghijklmnopqrstuvwxyz234567', true);
+    $encoded = $base32->encode($uuid);
+
+    $customdir = "/$encoded";
+    $path['path'] = str_replace($path['subdir'], '', $path['path']); //remove default year/month directory
+    $path['url'] = str_replace($path['subdir'], '', $path['url']);
+    $path['subdir'] = $customdir;
+    $path['path'] = $path['path'] . $customdir;
+    $path['url'] = $path['path'] . $customdir;
+    return $path;
+}
